31def8a0d8e952efd907a96f3dbf0b7e179e8a25
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-28 23:10:51 -0400

    + calling to convert markdown/README.md

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Markdig;
+using ShaderMarkdown.Rendering;
+
+var markdown = File.ReadAllText("markdown/README.md");
+
+var pipeline = new MarkdownPipelineBuilder()
+    .UseAdvancedExtensions()
+    .Build();
+
+var html = Markdown.ToHtml(markdown, pipeline);
+
+var shaderProcessor = new ShaderProcessor();
+
+var renderer = new HtmlRenderer(shaderProcessor);
+
+await renderer.RenderAsync(
+    html,
+    "output/README.png",
+    width: 1200,
+    height: 800,
+    scale: 2
+);
+
+Console.WriteLine("Rendered to output/");
