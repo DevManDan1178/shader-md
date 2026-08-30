eb91502fb1d8e25fa171125966f27ede2f0c2c46
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-30 16:46:46 -0400

    ~ modified

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using Markdig;
 using ShaderMarkdown.Rendering;
 
-var markdown = File.ReadAllText("markdown/README.md");
+var markdown = File.ReadAllText("markdown/TestSuite.md");
 
 var pipeline = new MarkdownPipelineBuilder()
     .UseAdvancedExtensions()
@@ -11,19 +11,22 @@ var html = Markdown.ToHtml(markdown, pipeline);
 
 var shaderProcessor = new ShaderProcessor();
 
-var renderer = new HtmlRenderer(shaderProcessor);
+var renderer = new HtmlShaderRenderer(shaderProcessor);
+
+ShaderInfo backgroundShaderInfo = ShaderInfo.FromShaderFileName("test.frag"); 
+string outputPath = "output/TestSuite2.gif";
 
 await renderer.RenderAsync(
     html: html,
-    outputPath: "output/README.gif",
+    outputPath: outputPath,
     width: 800,
     height: 100,
-    fps: 15,
+    fps: 3,
     duration: 1,
-    scale: 2,
+    scale: 4,
     backgroundColor: " #0d1117",
-    backgroundShader: "test",
-    outerShader: "rainbow"
+    backgroundShaderInfo: backgroundShaderInfo,
+    outerShaderInfo: null 
 );
 
-Console.WriteLine("Rendered to output/");
+Console.WriteLine($"Rendered to {outputPath}");
