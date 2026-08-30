a635d7043efb9882579b22076be28294c1b82dc6
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-30 03:34:31 -0400

    ~ refactor

--- filepaths/FilePaths.cs
+++ filepaths/FilePaths.cs
@@ -7,11 +7,7 @@ namespace ShaderMarkdown.FilePaths {
         private static string GetWebScriptPath(string webScriptRelativePath) {
             return Path.Combine(AppContext.BaseDirectory, WEB_SCRIPT_PATH_ROOT, webScriptRelativePath);
         }
-        public readonly static string CREATE_DOCUMENT =  GetWebScriptPath("CreateDocument.js");
-        public readonly static string RENDER_SHADER = GetWebScriptPath("RenderShader.js");
-        public readonly static string CREATE_ELEMENT_BACKGROUND = GetWebScriptPath("CreateElementBackground.js");
-        public readonly static string REPLACE_ELEMENT_WITH_IMAGE = GetWebScriptPath("ReplaceElementWithImage.js");
-        public readonly static string CREATE_DOCUMENT_BACKGROUND = GetWebScriptPath("CreateDocumentBackground.js");
-        
+        public readonly static string DOCUMENT_FUNCTIONS = GetWebScriptPath("DocumentFunctions.js");
+        public readonly static string SHADER_RENDERER = GetWebScriptPath("ShaderRenderer.js");
     }
 }
