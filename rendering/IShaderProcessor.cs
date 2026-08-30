fb56f601b15b7a1529b057812eedac56a4e3bbcd
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-29 20:59:40 -0400

    + animated shader application, returning a list of frames where the shader is applied with lerped time from 0 to 1 with duration

--- rendering/IShaderProcessor.cs
+++ rendering/IShaderProcessor.cs
@@ -1,7 +1,66 @@
 using Microsoft.Playwright;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace ShaderMarkdown.Rendering;
-
+/// <summary>
+/// Interface containing functions to process shaders to images (byte[])
+/// </summary>
 public interface IShaderProcessor {
-    Task<byte[]> ApplyAsync(IPage page, byte[] image, string shader);
+    async Task<byte[][]> ApplyAnimatedAsync(IPage page, byte[] image, string shader, int framesPerSecond, float duration) {
+        if (framesPerSecond <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(framesPerSecond));
+        }
+        
+        if (duration <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(duration));
+        }
+            
+        int frameCount = (int) (framesPerSecond * duration);
+        byte[][] frames = new byte[frameCount][];
+        for (int frame = 0; frame < frameCount; ++frame) {
+            float time = (float) frame / (float) framesPerSecond;
+            var thisFrame = await ApplyAsync(page, image, shader, new ShaderParameters {
+                Time = time,
+            });
+            frames[frame] = thisFrame;
+        }
+
+        return frames;
+    }
+
+    async Task<byte[][]> ApplyAnimatedToRectAsync(IPage page, int width, int height, string shader, int framesPerSecond, float duration, string color = "#FFFFFF") {
+        if (width <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(width));
+        }
+
+        if (height <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(height));
+        }
+        
+        using var image = new Image<Rgba32>(width, height);
+        Console.WriteLine($"Shader background color: '{color}'");
+        var parsedColor = Color.ParseHex(color.Trim());
+
+        image.Mutate(ctx => ctx.BackgroundColor(parsedColor));
+
+        await using var stream = new MemoryStream();
+        await image.SaveAsPngAsync(stream);
+
+        return await ApplyAnimatedAsync(
+            page,
+            stream.ToArray(),
+            shader,
+            framesPerSecond,
+            duration
+        );
+    }
+
+    Task<byte[]> ApplyAsync(IPage page, byte[] image, string shader, ShaderParameters shaderParameters);
+}
+
+
+public class ShaderParameters {
+    public float Time { get; set; }
 }
