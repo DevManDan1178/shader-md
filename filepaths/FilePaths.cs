201759a4add3694b604afd6ddda3120c00bce383
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-29 20:52:03 -0400

    + file paths for scripts and shader directory

---
+++ filepaths/FilePaths.cs
@@ -0,0 +1,17 @@
+namespace ShaderMarkdown.FilePaths {
+    class Directories {
+        public readonly static string DEFAULT_SHADER_DIRECTORY = Path.Combine(AppContext.BaseDirectory, "shaders");
+    }
+    class WebScriptPaths {
+        const string WEB_SCRIPT_PATH_ROOT = "generated";
+        private static string GetWebScriptPath(string webScriptRelativePath) {
+            return Path.Combine(AppContext.BaseDirectory, WEB_SCRIPT_PATH_ROOT, webScriptRelativePath);
+        }
+        public readonly static string CREATE_DOCUMENT =  GetWebScriptPath("CreateDocument.js");
+        public readonly static string SHADER_RENDERER = GetWebScriptPath("ShaderRenderer.js");
+        public readonly static string CREATE_ELEMENT_BACKGROUND = GetWebScriptPath("CreateElementBackground.js");
+        public readonly static string REPLACE_ELEMENT_WITH_IMAGE = GetWebScriptPath("ReplaceElementWithImage.js");
+        public readonly static string CREATE_DOCUMENT_BACKGROUND = GetWebScriptPath("CreateDocumentBackground.js");
+        
+    }
+}
