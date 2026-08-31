935086292fd357e2b8c14f20437a8d74ecd4b232
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-31 00:06:49 -0400

    + timescale property instead of interpolatestime boolean

--- src/rendering/shader/data/ShaderParameters.cs
+++ src/rendering/shader/data/ShaderParameters.cs
@@ -3,18 +3,22 @@ using System.Text.Json;
 namespace ShaderMarkdown.Rendering;
 
 public class ShaderParameters {
-    public float Time { get; set; } = 0f;
-    public bool InterpolateTime { get; init; } = true; 
+    const string TIME_PARAMETER_KEY = "time";
+    const string TIMESCALE_PARAMETER_KEY = "timescale";
+    const float DEFAULT_TIME_PARAMETER = 0f;
+    const float DEFAULT_TIMESCALE_PARAMETER = 1.0f;
+    public float Time { get; init; } = DEFAULT_TIME_PARAMETER;
+    public float TimeScale { get; init;} = DEFAULT_TIMESCALE_PARAMETER; 
     public Dictionary<string, JsonElement> ShaderProperties { get; init; } = new();
 
+
     public ShaderParameters() {}
-    public ShaderParameters(float time) {
+    public ShaderParameters(float time, float timeScale, Dictionary<string, JsonElement> shaderProperties) {
         Time = time;
-        InterpolateTime = false;
+        TimeScale = timeScale;
+        ShaderProperties = shaderProperties;
     }
     
-
-
     public static ShaderParameters ParseShaderParameters(string? json) {
         if (string.IsNullOrEmpty(json)) {
             return new ShaderParameters();
@@ -25,17 +29,20 @@ public class ShaderParameters {
             return new ShaderParameters();
         }
         
-        var time = parameters.GetValueOrDefault("time");
-        parameters.Remove("time");
+        var timeValue = parameters.GetValueOrDefault(TIME_PARAMETER_KEY);
+        parameters.Remove(TIME_PARAMETER_KEY);
 
+        var timeScaleValue = parameters.GetValueOrDefault(TIMESCALE_PARAMETER_KEY);
+        parameters.Remove(TIMESCALE_PARAMETER_KEY);
 
-        return time.ValueKind == JsonValueKind.Number
-            ?   new ((float) time.GetDouble()){
-                    ShaderProperties = parameters
-                }
-            :   new () {
-                ShaderProperties = parameters
-                };
-       
+        float time = timeValue.ValueKind == JsonValueKind.Number
+            ? (float) timeValue.GetDouble()
+            : DEFAULT_TIME_PARAMETER;
+
+        float timeScale = timeScaleValue.ValueKind == JsonValueKind.Number
+            ? (float) timeValue.GetDouble()
+            : DEFAULT_TIMESCALE_PARAMETER;
+
+        return new ShaderParameters(time, timeScale, parameters){};
     }
 }
