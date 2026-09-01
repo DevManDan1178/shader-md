64c07aa93a24e9769edc3609ae75225edd68641b
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-01 02:15:04 -0400

    + static shader renderer script path

--- src/filepaths/FilePaths.cs
+++ src/filepaths/FilePaths.cs
@@ -4,10 +4,16 @@ namespace ShaderMarkdown.FilePaths {
     }
     class WebScriptPaths {
         const string WEB_SCRIPT_PATH_ROOT = "generated";
-        private static string GetWebScriptPath(string webScriptRelativePath) {
-            return Path.Combine(AppContext.BaseDirectory, WEB_SCRIPT_PATH_ROOT, webScriptRelativePath);
+        private static string GetWebScriptPath(string webScriptFileName, params string[] webScriptSubdirectories) {
+            return Path.Combine([
+                AppContext.BaseDirectory, 
+                WEB_SCRIPT_PATH_ROOT, 
+                ..webScriptSubdirectories, 
+                webScriptFileName
+            ]);
         }
         public readonly static string DOCUMENT_FUNCTIONS = GetWebScriptPath("DocumentFunctions.js");
         public readonly static string SHADER_RENDERER = GetWebScriptPath("ShaderRenderer.js");
+        public readonly static string STATIC_SHADER_RENDERER = GetWebScriptPath("StaticShaderRenderer.js");
     }
 }
