dac9dbd4fc6043b0ba56026daf827095c1c65462
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-02 03:54:21 -0400

    ~ adapted startup

--- src/Program.cs
+++ src/Program.cs
@@ -1,38 +1,76 @@
 ﻿using Markdig;
 using ShaderMarkdown.Rendering;
+using ShaderMarkdown.FilePaths;
+using ShaderMarkdown.Config;
 
 string inputPath = "markdown/TestSuite.md";
 string outputPath = "output/TestSuite2.gif";
 
+/*
+ShaderInfo? backgroundShaderInfo = ShaderInfo.FromShaderFileName(shaderRootDirectory, "driftingSquares.frag", new ShaderParameters(0.0f, 0.01f, new()));
 
-ShaderInfo? backgroundShaderInfo = ShaderInfo.FromShaderFileName("driftingSquares.frag", new ShaderParameters(0.0f, 0.01f, new ())); 
-ShaderInfo? outerShaderInfo = ShaderInfo.FromShaderFileName("halfBright.frag", new());
+ShaderInfo? finalizeShaderInfo = null; //ShaderInfo.FromShaderFileName(shaderRootDirectory,"halfBright.frag", new());
+*/
+var parameters = new ShaderizeDocumentParameters() {
+    Paths = new ShaderizeDocumentParameters.IOPaths {
+        Input = inputPath,
+        Output = outputPath,
+    },
+    DocRenderSettings = new ShaderizeDocumentParameters.RenderSettings  {
+        DocSize = new ShaderizeDocumentParameters.RenderSettings.DocumentSize {
+            Width = 800,
+            Height = 100
+        }
+    },
+    FPS = 10,
+};
+string shaderConfigPath = Path.Combine(AppContext.BaseDirectory, "shaderConfig.yaml");
+if (!File.Exists(shaderConfigPath)) {
+    throw new FileNotFoundException($"Shader config not found: {shaderConfigPath}.");
+}
 
-var markdown = File.ReadAllText(inputPath);
+ShaderConfig shaderConfig;
+shaderConfig = ShaderConfig.ReadFromYAML(File.ReadAllText(shaderConfigPath));
 
-var pipeline = new MarkdownPipelineBuilder()
-    .UseAdvancedExtensions()
-    .Build();
-
-var html = Markdown.ToHtml(markdown, pipeline);
-
-var shaderProcessor = new ShaderProcessor();
-var renderer = new HtmlShaderRenderer(shaderProcessor);
-
+ 
 var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-await renderer.RenderAsync(
-    html: html,
-    outputPath: outputPath,
-    width: 800,
-    height: 100,
-    fps: 10,
-    duration: 1,
-    scale: 4,
-    backgroundColor: " #0d1117",
-    backgroundShaderInfo: backgroundShaderInfo,
-    outerShaderInfo: outerShaderInfo
+
+Console.WriteLine("Preparing to shaderize.");
+
+await ShaderizeDocument(
+    parameters,
+    shaderConfig
 );
+
 stopwatch.Stop();
 
+Console.WriteLine($"Shaderized {inputPath} to {outputPath} " + $"in {stopwatch.Elapsed.TotalSeconds:F2} seconds.");
 
-Console.WriteLine($"Shaderized {inputPath} to {outputPath} in {stopwatch.Elapsed.TotalSeconds:F2} seconds.");
+
+static async Task ShaderizeDocument(ShaderizeDocumentParameters parameters, ShaderConfig shaderConfig) {
+    string? markdown = File.ReadAllText(
+        parameters.Paths.Input
+    );
+
+    var pipeline = new MarkdownPipelineBuilder()
+        .UseAdvancedExtensions()
+        .Build();
+
+    string? html = Markdown.ToHtml(markdown, pipeline);
+
+    var shaderProcessor = new ShaderProcessor();
+
+    var renderer = new HtmlShaderRenderer(shaderProcessor);
+
+    await renderer.RenderAsync(
+        html: html,
+        shaderConfig,
+        outputPath: parameters.Paths.Output,
+        width: parameters.DocRenderSettings.DocSize.Width,
+        height: parameters.DocRenderSettings.DocSize.Height,
+        fps: parameters.FPS,
+        duration: parameters.Duration,
+        scale: parameters.Scale  
+    );
+}
+
