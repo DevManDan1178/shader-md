a58f051d8ec980ddff7de81f19b347021f0ef760
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-02 03:56:21 -0400

    ~ format

--- src/filepaths/FilePaths.cs
+++ src/filepaths/FilePaths.cs
@@ -1,19 +1,18 @@
-namespace ShaderMarkdown.FilePaths {
-    class Directories {
-        public readonly static string DEFAULT_SHADER_DIRECTORY = Path.Combine(AppContext.BaseDirectory, "shaders");
+namespace ShaderMarkdown.FilePaths;
+class Directories {
+    public readonly static string DEFAULT_SHADER_DIRECTORY = Path.Combine(AppContext.BaseDirectory, "shaders");
+}
+class WebScriptPaths {
+    const string WEB_SCRIPT_PATH_ROOT = "generated";
+    private static string GetWebScriptPath(string webScriptFileName, params string[] webScriptSubdirectories) {
+        return Path.Combine([
+            AppContext.BaseDirectory, 
+            WEB_SCRIPT_PATH_ROOT, 
+            ..webScriptSubdirectories, 
+            webScriptFileName
+        ]);
     }
-    class WebScriptPaths {
-        const string WEB_SCRIPT_PATH_ROOT = "generated";
-        private static string GetWebScriptPath(string webScriptFileName, params string[] webScriptSubdirectories) {
-            return Path.Combine([
-                AppContext.BaseDirectory, 
-                WEB_SCRIPT_PATH_ROOT, 
-                ..webScriptSubdirectories, 
-                webScriptFileName
-            ]);
-        }
-        public readonly static string DOCUMENT_FUNCTIONS = GetWebScriptPath("DocumentFunctions.js");
-        public readonly static string SHADER_RENDERER = GetWebScriptPath("ShaderRenderer.js");
-        public readonly static string STATIC_SHADER_RENDERER = GetWebScriptPath("StaticShaderRenderer.js");
-    }
+    public readonly static string DOCUMENT_FUNCTIONS = GetWebScriptPath("DocumentFunctions.js");
+    public readonly static string SHADER_RENDERER = GetWebScriptPath("ShaderRenderer.js");
+    public readonly static string STATIC_SHADER_RENDERER = GetWebScriptPath("StaticShaderRenderer.js");
 }
