86dc7949119887d51d8777e0482053a9f0cd69ae
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-02 03:11:23 -0400

    + serializable shader info

--- src/rendering/shader/data/ShaderInfo.cs
+++ src/rendering/shader/data/ShaderInfo.cs
@@ -1,15 +1,16 @@
+using System.Text.Json;
+
 namespace ShaderMarkdown.Rendering;
 
 public class ShaderInfo {
     public string ShaderPath { get; init; }
     public ShaderParameters ShaderParameters { get; set; } = new();
 
-    public static string ShaderDirectory = FilePaths.Directories.DEFAULT_SHADER_DIRECTORY;
-    
+
     private ShaderInfo(string shaderPath) {
         ShaderPath = shaderPath;
     }
-    private ShaderInfo(string shaderPath, ShaderParameters shaderParameters) : this(shaderPath) {
+    public ShaderInfo(string shaderPath, ShaderParameters shaderParameters) : this(shaderPath) {
         ShaderParameters = shaderParameters;
     }
 
@@ -17,11 +18,25 @@ public class ShaderInfo {
         return new (ShaderPath, shaderParameters);
     }
 
-    public static ShaderInfo FromShaderFileName(string shaderName) {
-        return new (Path.Combine(ShaderDirectory, $"{shaderName}"));
+    public static ShaderInfo FromShaderFileName(string shadersRootDirectory, string shaderName) {
+        return new (Path.Combine(shadersRootDirectory, $"{shaderName}"));
     }
 
-    public static ShaderInfo FromShaderFileName(string shaderName, ShaderParameters shaderParameters) {
-        return new (Path.Combine(ShaderDirectory, $"{shaderName}"), shaderParameters);
+    public static ShaderInfo FromShaderFileName(string shadersRootDirectory, string shaderName, ShaderParameters shaderParameters) {
+        return new (Path.Combine(shadersRootDirectory, $"{shaderName}"), shaderParameters);
     }
 }
+
+public class SerializableShaderInfo {
+    public string? ShaderPath { get; init; }
+    public Dictionary<string, JsonElement>? ShaderParameters { get; init; }
+
+    public ShaderInfo ToShaderInfo(string shadersRootDirectory) {
+        return new ShaderInfo(
+            ShaderPath?.Trim().Length > 0
+                ? Path.Combine(shadersRootDirectory, $"{ShaderPath}")
+                : "", 
+            Rendering.ShaderParameters.ParseShaderParameters(ShaderParameters)
+        );
+    }
+};
