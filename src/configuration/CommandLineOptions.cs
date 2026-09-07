52754e350d71147542fc353e13628c63f396bb4e
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-07 03:13:12 -0400

    + background color, overwriting existing file, output file extension, vertical slice out

--- src/configuration/CommandLineOptions.cs
+++ src/configuration/CommandLineOptions.cs
@@ -5,22 +5,27 @@ public class CommandLineOptions {
     const int DEFAULT_WIDTH = 800;
     const int DEFAULT_HEIGHT = 0;
     const int DEFAULT_FPS = 5;
+    const int DEFAULT_VERTICAL_SLICE_COUNT = 1;
     const float DEFAULT_SCALE = 1f;
     const float DEFAULT_DURATION = 1f;
+    const string DEFAULT_BACKGROUND_COLOR = "#0d1117";
     const AnimatedFileExtension DEFAULT_OUTPUT_EXTENSION = AnimatedFileExtension.WEBP;
     const bool DEFAULT_REVERSE_LOOP_FROM_END = false;
+    const bool DEFAULT_OVERWRITE_EXISTING_FILE = false;
     public required FileSystemInfo Input { get; init; }
     public required FileInfo ShaderConfig { get; init; }
     public required string Output { get; init; }
 
+    public string BackgroundColor { get; init; } = DEFAULT_BACKGROUND_COLOR;
+    public float Scale { get; init; } = DEFAULT_SCALE;
+    public float Duration { get; init; } = DEFAULT_DURATION;
     public int Width { get; init; } = DEFAULT_WIDTH;
     public int Height { get; init; } = DEFAULT_HEIGHT;
     public int FPS { get; init; } = DEFAULT_FPS;
-
-    public float Scale { get; init; } = DEFAULT_SCALE;
-    public float Duration { get; init; } = DEFAULT_DURATION;
-    public AnimatedFileExtension outputExtension { get; init; } = DEFAULT_OUTPUT_EXTENSION;
+    public int VerticalSliceCount { get; init; } = DEFAULT_VERTICAL_SLICE_COUNT;
+    public AnimatedFileExtension OutputExtension { get; init; } = DEFAULT_OUTPUT_EXTENSION;
     public bool ReverseLoopFromEnd { get; init; } = DEFAULT_REVERSE_LOOP_FROM_END;
+    public bool OverwriteExistingFile { get; init; } = DEFAULT_OVERWRITE_EXISTING_FILE;
 
     public static CommandLineOptions? ParseCommandLineArgs(string[] args) {
         if (args.Contains("--help") || args.Contains("-h") || args.Contains("-?")) {
@@ -53,6 +58,11 @@ public class CommandLineOptions {
             DefaultValueFactory = _ => DEFAULT_OUTPUT_EXTENSION
         };
 
+        var backgroundColorOption = new Option<string>("--bgcolor") {
+            Description = "Document background color in hex format (\"#xxxxxx\" or \"transparent\")",
+            DefaultValueFactory = _ => DEFAULT_BACKGROUND_COLOR,
+        };
+
         var widthOption = new Option<int>("--width") {
             Description = "Document width in pixels.",
             DefaultValueFactory = _ => DEFAULT_WIDTH
@@ -78,9 +88,19 @@ public class CommandLineOptions {
             DefaultValueFactory = _ => DEFAULT_DURATION
         };
 
+        var verticalSliceOption = new Option<int>("--vslices") {
+            Description = "Amount of vertical slices to separate the output to",
+            DefaultValueFactory = _ => DEFAULT_VERTICAL_SLICE_COUNT
+        };
+
         var reverseOption = new Option<bool>("--reverseloop") {
             Description = "Reverse the animation after ending for seamless looping.",
             DefaultValueFactory = _ => DEFAULT_REVERSE_LOOP_FROM_END
+        };
+
+        var overwriteExistingOption = new Option<bool>("--oef") {
+            Description = "Overwrites the existing file at the output location if it exists.",
+            DefaultValueFactory = _ => DEFAULT_OVERWRITE_EXISTING_FILE,
         };
 
         var rootCommand = new RootCommand("Converts a Markdown document into a shaderized document.");
@@ -95,6 +115,9 @@ public class CommandLineOptions {
         rootCommand.Options.Add(durationOption);
         rootCommand.Options.Add(reverseOption);
         rootCommand.Options.Add(outputExtensionOption);
+        rootCommand.Options.Add(backgroundColorOption);
+        rootCommand.Options.Add(verticalSliceOption);
+        rootCommand.Options.Add(overwriteExistingOption);
 
         var parseResult = rootCommand.Parse(args);
 
@@ -112,7 +135,10 @@ public class CommandLineOptions {
             Scale = parseResult.GetValue(scaleOption),
             Duration = parseResult.GetValue(durationOption),
             ReverseLoopFromEnd = parseResult.GetValue(reverseOption),
-            outputExtension = parseResult.GetValue(outputExtensionOption),
+            OutputExtension = parseResult.GetValue(outputExtensionOption),
+            VerticalSliceCount = parseResult.GetValue(verticalSliceOption),
+            OverwriteExistingFile = parseResult.GetValue(overwriteExistingOption),
+            BackgroundColor = parseResult.GetValue(backgroundColorOption) ?? DEFAULT_BACKGROUND_COLOR,
         };
     }
 
@@ -131,8 +157,11 @@ public class CommandLineOptions {
                 --fps <value>       Frames per second. [default: {DEFAULT_FPS}]
                 --scale <value>     Render scale. [default: {DEFAULT_SCALE}]
                 --duration <value>  Animation duration in seconds. [default: {DEFAULT_DURATION}]
-                --outputext         File extension of the outputted shaderized documents [default: {DEFAULT_OUTPUT_EXTENSION}]
                 --reverseloop       Reverse the animation between bounds for seamless looping.
+                --bgcolor <value>   Background color of the document in hex format ({"\"#xxxxxx\" or \"transparent\""}) [default: {DEFAULT_BACKGROUND_COLOR}]
+                --vslices <value>   The ammount of vertical slices to divide the output into. If over 1, documents export to a directory containing each slice. [default: {DEFAULT_VERTICAL_SLICE_COUNT}]
+                --outputext <value> File extension of the outputted shaderized documents [default: {DEFAULT_OUTPUT_EXTENSION}]
+                --oef                Overwrites the existing file at the output location if it exists. When disabled, a file at the output path halts the process. 
 
             -h, --help              Show help and usage information.
         """;
