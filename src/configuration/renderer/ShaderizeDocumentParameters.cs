e6d498f8ec37d893fc7039cf12be8f7a22a6a7b6
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-07 03:14:06 -0400

    + all fields required

--- src/configuration/renderer/ShaderizeDocumentParameters.cs
+++ src/configuration/renderer/ShaderizeDocumentParameters.cs
@@ -5,17 +5,17 @@ namespace ShaderMarkdown.Config;
 public class ShaderizeDocumentParameters {
     public class RenderSettings {
         public class DocumentSize {
-            public int Width { get; init; } = 800;
-            public int Height { get; init; } = 0;
+            public required int Width { get; init; }
+            public required int Height { get; init; }
         }
 
-        public DocumentSize DocSize { get; init; } = new();
+        public required DocumentSize DocSize { get; init; }
     }
 
-    public RenderSettings DocRenderSettings { get; init; } = new();
-
-    public float Duration { get; init; } = 1.0f;
-    public float Scale { get; init; } = 1.0f;
-    public int FPS { get; init; } = 6;
-    public bool ReverseLoopFromEnd { get; init; } = false;
+    public required RenderSettings DocRenderSettings { get; init; }
+    public required string BackgroundColor { get; init; }
+    public required float Duration { get; init; } 
+    public required float Scale { get; init; } 
+    public required int FPS { get; init; } 
+    public required bool ReverseLoopFromEnd { get; init; }
 }
