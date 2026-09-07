8f0179836b420bd0496061bd81a321fb24d1a70a
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-06 22:22:29 -0400

    + output file extension as argument, setting filename to include file extension if missing

--- src/exporting/AnimatedExporter.cs
+++ src/exporting/AnimatedExporter.cs
@@ -10,8 +10,8 @@ public static class AnimatedExporter {
     /// <param name="frames">frames to export</param>
     /// <param name="fps"></param>FPS of the animation
     /// <param name="outputPath">Output path of the exported animation</param>
-    /// <returns>(bool: success status, string: possible error message)</returns>
-    public static async Task<(bool, string?)> ExportAnimatedAsync(IReadOnlyList<byte[]> frames, int fps, string outputPath) {
+    /// <returns>The output path (if successful)</returns>
+    public static async Task<string?> ExportAnimatedAsync(IReadOnlyList<byte[]> frames, int fps, string outputPath, AnimatedFileExtension outputExtension) {
         if (frames.Count == 0) {
             throw new ArgumentException("No frames were provided.", nameof(frames));
         }
@@ -21,23 +21,26 @@ public static class AnimatedExporter {
         }
         
         AnimatedFileExtension? fileExtension = FileExtension.GetAnimatedFileExtension(outputPath);
-        if (fileExtension == null) {
-            return (false, $"Unsupported file extension: \"{outputPath}\".");
+        if (fileExtension != outputExtension) {
+            string? fileExtensionStr = FileExtension.GetFileExtensionString(outputExtension);
+            if (fileExtensionStr != null) {
+                outputPath += $".{fileExtensionStr}";
+            }
         }
-        switch (fileExtension) {
+        switch (outputExtension) {
             case AnimatedFileExtension.GIF:
                 await GifBuilder.SaveAsync(frames, fps, outputPath);
-                return (true, "");
+                break;
             case AnimatedFileExtension.WEBP:
                 await WebPBuilder.SaveAsync(frames, fps, outputPath);
-                return (true, "");
+                break;
             case AnimatedFileExtension.APNG:
                 await APNGBuilder.SaveAsync(frames, fps, outputPath);
-                return (true, "");
+                break;
             default:
                 break;
         }
 
-        return (true, "");
+        return outputPath;
     }
 }
