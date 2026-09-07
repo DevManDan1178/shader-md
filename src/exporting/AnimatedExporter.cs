0dd9a173e16c9790a534b3878892d9af676778a6
DevManDan1178 <l3r0d4niel@gmail.com>
2026-09-07 03:12:33 -0400

    - removed potential output path changes

--- src/exporting/AnimatedExporter.cs
+++ src/exporting/AnimatedExporter.cs
@@ -1,4 +1,7 @@
 using ShaderMarkdown.Files;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace ShaderMarkdown.Exporting;
 
@@ -10,8 +13,8 @@ public static class AnimatedExporter {
     /// <param name="frames">frames to export</param>
     /// <param name="fps"></param>FPS of the animation
     /// <param name="outputPath">Output path of the exported animation</param>
-    /// <returns>The output path (if successful)</returns>
-    public static async Task<string?> ExportAnimatedAsync(IReadOnlyList<byte[]> frames, int fps, string outputPath, AnimatedFileExtension outputExtension) {
+    /// <returns>Success status</returns>
+    public static async Task ExportAnimatedAsync(IReadOnlyList<byte[]> frames, int fps, string outputPath, AnimatedFileExtension outputExtension) {
         if (frames.Count == 0) {
             throw new ArgumentException("No frames were provided.", nameof(frames));
         }
@@ -20,13 +23,6 @@ public static class AnimatedExporter {
             throw new ArgumentOutOfRangeException(nameof(fps), "FPS must be greater than zero.");
         }
         
-        AnimatedFileExtension? fileExtension = FileExtension.GetAnimatedFileExtension(outputPath);
-        if (fileExtension != outputExtension) {
-            string? fileExtensionStr = FileExtension.GetFileExtensionString(outputExtension);
-            if (fileExtensionStr != null) {
-                outputPath += $".{fileExtensionStr}";
-            }
-        }
         switch (outputExtension) {
             case AnimatedFileExtension.GIF:
                 await GifBuilder.SaveAsync(frames, fps, outputPath);
@@ -40,7 +36,5 @@ public static class AnimatedExporter {
             default:
                 break;
         }
-
-        return outputPath;
     }
 }
